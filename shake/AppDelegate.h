6742e1f5485869cdb99f669e5f1e261191144dcb
divya136 <sathish@cupertino.local>
2015-03-11 17:54:45 +0530

    Initial Commit

---
+++ shake/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shake
+//
+//  Created by gunasundari on 11/03/15.
+//  Copyright (c) 2015 XERAGO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shake/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  shake
+//
+//  Created by gunasundari on 11/03/15.
+//  Copyright (c) 2015 XERAGO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
